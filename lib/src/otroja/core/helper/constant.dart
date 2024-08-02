f94baa983aab3ef4b22e90d914218fe5ffb4dcd6
abd <abdalltefchtourou@gmail.com>
2024-08-02 11:19:17 +0300

    edit

--- lib/src/otroja/core/helper/constant.dart
+++ lib/src/otroja/core/helper/constant.dart
@@ -3,4 +3,4 @@ import 'funtions.dart';
 final dimensions = new Dimensions();
 String ip="192.168.1.102";
  String studentId='';
-String baseUrl="https://de1b-185-107-56-70.ngrok-free.app/api/";
+String baseUrl="http://$ip:8000/api/";
